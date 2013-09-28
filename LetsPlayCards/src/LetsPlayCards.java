e4dfeaa8a618449d931e4be96923abe679e720e0
FreeWillyWonka <imshorty8@msn.com>
2013-09-27 23:14:29 -0700

    Initial Commit

---
+++ LetsPlayCards/src/LetsPlayCards.java
@@ -0,0 +1,8 @@
+
+public class LetsPlayCards {
+	public static void main(String[] args)
+	{
+		Welcome w = new Welcome();
+		w.WelcomScreen();
+	}
+}
---
+++ LetsPlayCards/src/Welcome.java
@@ -0,0 +1,19 @@
+
+public class Welcome {
+	public void WelcomScreen()
+	{
+		System.out.println("--------- Welcome to Let's Play Cards! -----------");
+		System.out.println("|                                                |");
+		System.out.println("|   1. Deck                                      |");
+		System.out.println("|                                                |");
+		System.out.println("|   2. Discard                                   |");
+		System.out.println("|                                                |");
+		System.out.println("|   3. Hand                                      |");
+		System.out.println("|                                                |");
+		System.out.println("|   4. Field/IP                                  |");
+		System.out.println("|                                                |");
+		System.out.println("|   5. Trash/OOP                                 |");
+		System.out.println("|                                                |");
+		System.out.println("|------------------------------------------------|");
+	}
+}
